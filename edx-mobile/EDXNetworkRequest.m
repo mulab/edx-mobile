fa1ae4284447f97c03de422f21f06e31cb0207b4
Guang Chen <cgcgbcbc@163.com>
2013-11-12 03:54:50 +0800

    init NetworkRequest class

---
+++ edx-mobile/EDXNetworkRequest.m
@@ -0,0 +1,13 @@
+//
+//  EDXNetworkRequest.m
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import "EDXNetworkRequest.h"
+
+@implementation EDXNetworkRequest
+
+@end
