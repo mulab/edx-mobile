3178543d06a931ed0ac494a6c86730b74bec2c8e
Guang Chen <cgcgbcbc@163.com>
2013-11-12 01:48:19 +0800

    init login view

---
+++ edx-mobile/EDXLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  EDXLoginViewController.h
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDXLoginViewController : UIViewController
+
+@end
---
+++ edx-mobile/EDXLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  EDXLoginViewController.m
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import "EDXLoginViewController.h"
+
+@interface EDXLoginViewController ()
+
+@end
+
+@implementation EDXLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
