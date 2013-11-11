08fe6091ee7e6a4b3fc2ee702898aeeae491328b
Guang Chen <cgcgbcbc@163.com>
2013-11-12 01:45:00 +0800

    Initial Commit

---
+++ edx-mobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EDXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EDXAppDelegate class]));
+    }
+}
---
+++ edx-mobileTests/edx_mobileTests.m
@@ -0,0 +1,32 @@
+//
+//  edx_mobileTests.m
+//  edx-mobileTests
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import "edx_mobileTests.h"
+
+@implementation edx_mobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in edx-mobileTests");
+}
+
+@end
