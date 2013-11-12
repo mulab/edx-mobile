1eb76236f0c9c7e7f1e2e1d360802728c33363e7
Guang Chen <cgcgbcbc@163.com>
2013-11-12 19:26:12 +0800

    constants for request url

---
+++ edx-mobile/EDXConstants.h
@@ -0,0 +1,13 @@
+//
+//  EDXConstants.h
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EDXConstants : NSObject
+
+@end
---
+++ edx-mobile/EDXConstants.m
@@ -0,0 +1,13 @@
+//
+//  EDXConstants.m
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import "EDXConstants.h"
+
+@implementation EDXConstants
+
+@end
