3d3395bdac1f3efe827d856f2259885d09739470
Guang Chen <cgcgbcbc@163.com>
2013-11-12 18:47:40 +0800

    add network class and delegate

---
+++ edx-mobile/EDXNetwork.h
@@ -0,0 +1,13 @@
+//
+//  EDXNetwork.h
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EDXNetwork : NSObject
+
+@end
---
+++ edx-mobile/EDXNetwork.m
@@ -0,0 +1,13 @@
+//
+//  EDXNetwork.m
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import "EDXNetwork.h"
+
+@implementation EDXNetwork
+
+@end
