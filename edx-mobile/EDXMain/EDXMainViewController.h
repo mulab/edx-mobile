22c76f1865465b6c07bece6df0ac33ed285266c8
Guang Chen <cgcgbcbc@163.com>
2013-11-12 21:28:29 +0800

    add main controller

---
+++ edx-mobile/EDXMain/EDXMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  EDXMainViewController.h
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDXMainViewController : UIViewController
+
+@end
---
+++ edx-mobile/EDXMain/EDXMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  EDXMainViewController.m
+//  edx-mobile
+//
+//  Created by cissy on 13-11-12.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import "EDXMainViewController.h"
+
+@interface EDXMainViewController ()
+
+@end
+
+@implementation EDXMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
