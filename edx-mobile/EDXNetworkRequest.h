29aaa550eac96a160b35f1b0b37a1372c42570da
Guang Chen <cgcgbcbc@163.com>
2013-11-12 18:48:21 +0800

    complete network request class

--- edx-mobile/EDXNetworkRequest.h
+++ edx-mobile/EDXNetworkRequest.h
@@ -7,7 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "EDXNetworkDelegate.h"
 @interface EDXNetworkRequest : NSObject
-
+@property (nonatomic,retain)id<EDXNetworkDelegate> owner;//who delegate the request
+@property (nonatomic,retain)NSString* url;//request url
+@property (assign)NSStringEncoding enc;//response encodeing
+@property (assign)kRequestStatus requestStatus;
+@property (assign)kBusinessTag businessTag;
+@property (nonatomic,readonly,getter = result)NSString* result;
+-(void)cancel;
+-(void)postJson: (NSString*)json delegate:(id)delegate;//post json method
 @end
--- edx-mobile/EDXNetworkRequest.m
+++ edx-mobile/EDXNetworkRequest.m
@@ -7,7 +7,56 @@
 //
 
 #import "EDXNetworkRequest.h"
-
+#import "ASIHTTPRequest.h"
 @implementation EDXNetworkRequest
+{
+    ASIHTTPRequest* _request;//use this object to do http request
+}
+@synthesize enc,url;
+@synthesize requestStatus;
+@synthesize businessTag;
+@synthesize owner;
 
+-(void)cancel{
+    if (_request!=nil) {
+        [_request release],_request=nil;
+    }
+}
+-(void)setOwner:(id<EDXNetworkDelegate>)_owner{
+    owner=[_owner retain];
+}
+-(id<EDXNetworkDelegate>)owner{
+    return owner;
+}
+-(NSString*)result{
+    if(requestStatus==kRequestStatusEnded){
+        NSData *data = [_request responseData];
+        NSString* string=[[NSString alloc] initWithData:data encoding:self.enc];
+        
+        return string;
+    }else
+        return nil;
+}
+-(void)postJson:(NSString*)json delegate:(id)delegate{
+    //cancle unfinished request
+    [self cancel];
+    _request =[[ASIHTTPRequest requestWithURL:[NSURL URLWithString:url]]retain];
+    [_request setShouldAttemptPersistentConnection:NO];//Do not reuse HTTP connection
+    [_request setResponseEncoding:self.enc];//set response encoding
+    NSMutableDictionary* reqHeaders = [[NSMutableDictionary alloc]init];
+    [reqHeaders setValue:@"application/json; charset=UTF-8" forKey:@"Content-Type"];
+    _request.requestHeaders = reqHeaders; // set header
+    [reqHeaders release];
+    NSLo(@"post json:%@",json);
+    _request.tag=self.businessTag;
+    [_request appendPostData:[json dataUsingEncoding:self.enc]];//set post data
+    [_request setDelegate:delegate];//set request delegate
+    requestStatus = kRequestStatusBeging;// request begin
+    [_request startAsynchronous];// start async request
+}
+-(void)dealloc{
+    [owner release],owner=nil;
+    [_request release];
+    [super dealloc];
+}
 @end
