23e07aeeec17e8ea29b37677986da468088c38ce
Guang Chen <cgcgbcbc@163.com>
2013-12-16 19:22:36 +0800

    add UIViewDelegate

---
+++ edx-mobile/EDXUIViewDelegate.h
@@ -0,0 +1,16 @@
+//
+//  EDXUIViewDelegate.h
+//  edx-mobile
+//
+//  Created by cissy on 13-12-16.
+//  Copyright (c) 2013年 mulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol EDXUIViewDelegate <NSObject>
+@optional
+-(void)BeforeRefreshView;
+-(void)RefreshView;
+-(void)RaiseRequest;
+@end
