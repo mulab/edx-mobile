143b5e686d90f18da1cfd465b189a1fb174015d4
cgcgbcbc <cgcgbcbc@163.com>
2014-01-13 17:24:25 +0800

    add test target

---
+++ edx-mobile Tests/edx_mobile_Tests.m
@@ -0,0 +1,34 @@
+//
+//  edx_mobile_Tests.m
+//  edx-mobile Tests
+//
+//  Created by Guang Chen on 1/13/14.
+//  Copyright (c) 2014 mulab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface edx_mobile_Tests : XCTestCase
+
+@end
+
+@implementation edx_mobile_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
