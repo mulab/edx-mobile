a5f1912567e8488b652537fbfa1067d502b1c4f0
cgcgbcbc <cgcgbcbc@163.com>
2014-01-13 17:01:47 +0800

    add EDXURLHelperTest from template

---
+++ EDXURLHelperTest.m
@@ -0,0 +1,34 @@
+//
+//  EDXURLHelperTest.m
+//  edx-mobile
+//
+//  Created by Guang Chen on 1/13/14.
+//  Copyright (c) 2014 mulab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EDXURLHelperTest : XCTestCase
+
+@end
+
+@implementation EDXURLHelperTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
