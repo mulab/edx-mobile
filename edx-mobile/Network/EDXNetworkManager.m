f208df7b23bd5000fe6a79ee27556365cd6c8d2d
cgcgbcbc <cgcgbcbc@163.com>
2014-02-06 21:55:20 +0800

    refactor EDXNetworkMagager

--- edx-mobile/Network/EDXNetworkManager.m
+++ edx-mobile/Network/EDXNetworkManager.m
@@ -6,7 +6,9 @@
 #import <Objection/Objection.h>
 #import "EDXNetworkManager.h"
 #import "AFHTTPRequestOperation.h"
-
+@interface EDXNetworkManager()
+- (void)startOperation:(AFHTTPRequestOperation *)operation ForOwner:(id <EDXNetworkDelegate>)owner;
+@end
 @implementation EDXNetworkManager {
 
 }
@@ -18,25 +20,23 @@ objection_requires(@"factory")
     AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     op.responseSerializer = [AFHTTPResponseSerializer serializer];
     op.responseSerializer.acceptableContentTypes= [NSSet setWithObjects:@"application/json", @"text/plain", nil];
-    [op setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject){
-        [owner success:responseObject business:kBusinessTagSignUp];
-    } failure:^(AFHTTPRequestOperation *operation,NSError *err){
-        [owner error:err];
-    }];
-    [[NSOperationQueue mainQueue] addOperation:op];
+    [self startOperation:op ForOwner:owner];
 }
-
 
 - (void)LoginWith:(EDXLoginData)data owner:(id <EDXNetworkDelegate>)owner {
     [owner before:kBusinessTagUserLogin];
     NSURLRequest *request = [factory AccessTokenRequestWithData:data];
     AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     op.responseSerializer = [AFJSONResponseSerializer serializer];
-    [op setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation,id responseObject){
+    [self startOperation:op ForOwner:owner];
+}
+
+- (void)startOperation:(AFHTTPRequestOperation *)operation ForOwner:(id <EDXNetworkDelegate>)owner {
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *op, id responseObject) {
         [owner success:responseObject business:kBusinessTagSignUp];
-    } failure:^(AFHTTPRequestOperation *operation,NSError *error){
-        [owner error:error];
+    }                                failure:^(AFHTTPRequestOperation *op, NSError *err) {
+        [owner error:err];
     }];
-    [[NSOperationQueue mainQueue] addOperation:op];
+    [[NSOperationQueue mainQueue] addOperation:operation];
 }
 @end
