3e1fbf0bb51bb0b327e07152c6192282e6cb361e
cgcgbcbc <cgcgbcbc@163.com>
2014-02-22 00:55:35 +0800

    create course detail model

---
+++ edx-mobile/UI/Model/EDXCourseDetailModel.m
@@ -0,0 +1,13 @@
+//
+//  EDXCourseDetailModel.m
+//  edx-mobile
+//
+//  Created by Guang Chen on 2/22/14.
+//  Copyright (c) 2014 mulab. All rights reserved.
+//
+
+#import "EDXCourseDetailModel.h"
+
+@implementation EDXCourseDetailModel
+
+@end
