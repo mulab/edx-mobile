71774f77adbb4875ce7828311ecb45c002467947
cgcgbcbc <cgcgbcbc@163.com>
2014-02-22 08:34:47 +0800

    create course detail view controller

---
+++ edx-mobile/EDXCourseDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EDXCourseDetailViewController.h
+//  edx-mobile
+//
+//  Created by Guang Chen on 2/22/14.
+//  Copyright (c) 2014 mulab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDXCourseDetailViewController : UISplitViewController
+
+@end
---
+++ edx-mobile/EDXCourseDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  EDXCourseDetailViewController.m
+//  edx-mobile
+//
+//  Created by Guang Chen on 2/22/14.
+//  Copyright (c) 2014 mulab. All rights reserved.
+//
+
+#import "EDXCourseDetailViewController.h"
+
+@interface EDXCourseDetailViewController ()
+
+@end
+
+@implementation EDXCourseDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
